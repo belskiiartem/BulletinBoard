09cf1292709d6c286f26d1d71db5939731c3afc4
Belskii Artem <belskiiartem@gmail.com>
2015-10-15 14:17:21 +0300

    interface

--- src/main/java/belskii/artem/bulletinboard/dao/category/CategoryDaoHiberImpl.java
+++ src/main/java/belskii/artem/bulletinboard/dao/category/CategoryDaoHiberImpl.java
@@ -1,5 +1,22 @@
 package belskii.artem.bulletinboard.dao.category;
 
+import antlr.collections.List;
+
 public class CategoryDaoHiberImpl implements CategoryDao{
+
+	public List getAllCategorues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void AddCategorues(String title) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeCategories(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
